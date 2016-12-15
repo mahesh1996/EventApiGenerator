5e21716a0a4a20c650da899a8607c0ae21f1c4c2
Mahesh Bhuva <maheshbhuva2@gmail.com>
2016-12-15 19:17:56 +0530

    Initial Commit

--- GenerateApi.java
+++ GenerateApi.java
@@ -1,62 +1,62 @@
 public class GenerateApi {
-	private static String name;
-	private static String userTableName;
-	private static int no;
-	public static void main(String[] args) {
-		if(args.length < 3)
-			printUsage();
-		else{
-			String[] first = args[0].split("=");
-			String[] second = args[1].split("=");
-			String[] third = args[2].split("=");
-			processArgs(first);
-			processArgs(second);
-			processArgs(third);
-		}
-		if(no > 1){
-			new MultiParticipate(name,no,userTableName).generateApi();
-		}
-		else if(no==1){
-			new SingleParticipate(name).generateApi();
-		}
-		else if(no < 1){
-			System.out.println("No can not be zero or negative");
-			printUsage();
-		}
-	}
-	
-	public static void printUsage() {
-		System.out.println("Usage : ");
-		System.out.println("    --name=xyz (name of the event table and file name will be same)");
-		System.out.println("    --userTableName=user (name of the table in which users' email and password is stored)");
-		System.out.println("    --no=3 (no of participant in this event)");
-	}
+  private static String name;
+  private static String userTableName;
+  private static int no;
+  public static void main(String[] args) {
+    if(args.length < 3)
+      printUsage();
+    else{
+      String[] first = args[0].split("=");
+      String[] second = args[1].split("=");
+      String[] third = args[2].split("=");
+      processArgs(first);
+      processArgs(second);
+      processArgs(third);
+    }
+    if(no > 1){
+      new MultiParticipate(name,no,userTableName).generateApi();
+    }
+    else if(no==1){
+      new SingleParticipate(name).generateApi();
+    }
+    else if(no < 1){
+      System.out.println("No can not be zero or negative");
+      printUsage();
+    }
+  }
+  
+  public static void printUsage() {
+    System.out.println("Usage : ");
+    System.out.println("    --name=xyz (name of the event table and file name will be same)");
+    System.out.println("    --userTableName=user (name of the table in which users' email and password is stored)");
+    System.out.println("    --no=3 (no of participant in this event)");
+  }
 
-	public static void processArgs(String[] args) {
-		if(args.length < 2)
-		{
-			printUsage();
-			return;
-		}
-		if(args[0].startsWith("--name")) {
-			if(args[1].length() > 0)
-				name = args[1];
-			else
-				printUsage();
-		}
-		else if(args[0].startsWith("--no")) {
-			if(args[1].length () > 0)
-				no = Integer.parseInt(args[1]);
-			else
-				printUsage();
-		}
-		else if(args[0].startsWith("--userTableName")) {
-			if(args[1].length() > 0)
-				userTableName = args[1];
-			else
-				printUsage();
-		}
-		else
-			printUsage();
-	}
+  public static void processArgs(String[] args) {
+    if(args.length < 2)
+    {
+      printUsage();
+      return;
+    }
+    if(args[0].startsWith("--name")) {
+      if(args[1].length() > 0)
+        name = args[1];
+      else
+        printUsage();
+    }
+    else if(args[0].startsWith("--no")) {
+      if(args[1].length () > 0)
+        no = Integer.parseInt(args[1]);
+      else
+        printUsage();
+    }
+    else if(args[0].startsWith("--userTableName")) {
+      if(args[1].length() > 0)
+        userTableName = args[1];
+      else
+        printUsage();
+    }
+    else
+      printUsage();
+  }
 }
--- SingleParticipate.java
+++ SingleParticipate.java
@@ -1,117 +1,117 @@
 import java.io.*;
 public class SingleParticipate {
-    private String table;
-    private static final String LOGGED_SESSION = "isLoggedIn";
-    private static final String ACTION = "action";
-    private static final String HOST = "$host";
-    private static final String DB = "$db";
-    private static final String DB_USER = "$dbuser";
-    private static final String DB_PASSWORD = "$dbpassword";
-    private StringBuilder data = new StringBuilder();
-    public SingleParticipate(String table) {
-        this.table = table;
-    }
+  private String table;
+  private static final String LOGGED_SESSION = "isLoggedIn";
+  private static final String ACTION = "action";
+  private static final String HOST = "$host";
+  private static final String DB = "$db";
+  private static final String DB_USER = "$dbuser";
+  private static final String DB_PASSWORD = "$dbpassword";
+  private StringBuilder data = new StringBuilder();
+  public SingleParticipate(String table) {
+    this.table = table;
+  }
 
-    /*public static void main(String args[]){
-        new SingleParticipate("user").generateApi();
-    }*/
+  /*public static void main(String args[]){
+    new SingleParticipate("user").generateApi();
+  }*/
 
-    public void generateApi() {
-        this.addPreProcessData();
-        this.addRegisterData();
-        this.addUnregisterData();
-        this.addStatusData();
-        try{
-            BufferedWriter bw = new BufferedWriter(new FileWriter(this.table + ".php"));
-            bw.write(this.data.toString());
-            bw.flush();
-        }
-        catch(Exception e){}
+  public void generateApi() {
+    this.addPreProcessData();
+    this.addRegisterData();
+    this.addUnregisterData();
+    this.addStatusData();
+    try{
+      BufferedWriter bw = new BufferedWriter(new FileWriter(this.table + ".php"));
+      bw.write(this.data.toString());
+      bw.flush();
     }
+    catch(Exception e){}
+  }
 
-    public void addPreProcessData() {
-        this.data.append("<?php\n")
-        .append("session_start();\n")
-        .append("if(isset($_SESSION['"+ LOGGED_SESSION +"']) && !empty($_SESSION['"+ LOGGED_SESSION +"'])){\n")
-        .append("    $email = $_SESSION['"+ LOGGED_SESSION +"'];\n")
-        .append("    if(isset($_GET['"+ ACTION +"']) && !empty($_GET['"+ ACTION +"'])){\n")
-        .append("        $status = $_GET['"+ ACTION +"'] ;\n");
-    }
+  public void addPreProcessData() {
+    this.data.append("<?php\n")
+    .append("session_start();\n")
+    .append("if(isset($_SESSION['"+ LOGGED_SESSION +"']) && !empty($_SESSION['"+ LOGGED_SESSION +"'])){\n")
+    .append("    $email = $_SESSION['"+ LOGGED_SESSION +"'];\n")
+    .append("    if(isset($_GET['"+ ACTION +"']) && !empty($_GET['"+ ACTION +"'])){\n")
+    .append("        $status = $_GET['"+ ACTION +"'] ;\n");
+  }
 
-    public void addRegisterData() {
-        this.data.append("        if($status == 'register'){").append("\n")
-        .append("            try{").append("\n")
-        .append("                $con = new PDO('mysql:host='."+HOST+".';dbname='."+DB+","+DB_USER+","+DB_PASSWORD+");").append("\n")
-        .append("                $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);").append("\n")
-        .append("                $check_query = $con->prepare(\"select * from "+ this.table +" where email1='\".$email.\"'\");").append("\n")
-        .append("                $check_query->execute();").append("\n")
-        .append("                if($check_query->rowCount() == 0){").append("\n")
-        .append("                    $register_query = $con->prepare(\"insert into "+this.table+"(email1) values(:email)\");").append("\n")
-        .append("                    $register_query->bindParam(':email', $email);").append("\n")
-        .append("                    $status = $register_query->execute();").append("\n")
-        .append("                    if($status){").append("\n")
-        .append("                        echo json_encode( array('status' => 'registered'));").append("\n").append("\n")
-        .append("                    }").append("\n")
-        .append("                    else").append("\n")
-        .append("                        echo json_encode( array('status' => 'error'));").append("\n")
-        .append("                }").append("\n")
-        .append("                else if($check_query->rowCount() > 0) {").append("\n")
-        .append("                    echo json_encode( array('status' => 'registered'));").append("\n")
-        .append("                }").append("\n")
-        .append("            }").append("\n")
-        .append("            catch(PDOException $e){").append("\n")
-        .append("                echo $e->getMessage();").append("\n")
-        .append("            }").append("\n")
-        .append("        }").append("\n");
-    }
+  public void addRegisterData() {
+    this.data.append("        if($status == 'register'){").append("\n")
+    .append("            try{").append("\n")
+    .append("                $con = new PDO('mysql:host='."+HOST+".';dbname='."+DB+","+DB_USER+","+DB_PASSWORD+");").append("\n")
+    .append("                $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);").append("\n")
+    .append("                $check_query = $con->prepare(\"select * from "+ this.table +" where email1='\".$email.\"'\");").append("\n")
+    .append("                $check_query->execute();").append("\n")
+    .append("                if($check_query->rowCount() == 0){").append("\n")
+    .append("                    $register_query = $con->prepare(\"insert into "+this.table+"(email1) values(:email)\");").append("\n")
+    .append("                    $register_query->bindParam(':email', $email);").append("\n")
+    .append("                    $status = $register_query->execute();").append("\n")
+    .append("                    if($status){").append("\n")
+    .append("                        echo json_encode( array('status' => 'registered'));").append("\n").append("\n")
+    .append("                    }").append("\n")
+    .append("                    else").append("\n")
+    .append("                        echo json_encode( array('status' => 'error'));").append("\n")
+    .append("                }").append("\n")
+    .append("                else if($check_query->rowCount() > 0) {").append("\n")
+    .append("                    echo json_encode( array('status' => 'registered'));").append("\n")
+    .append("                }").append("\n")
+    .append("            }").append("\n")
+    .append("            catch(PDOException $e){").append("\n")
+    .append("                echo $e->getMessage();").append("\n")
+    .append("            }").append("\n")
+    .append("        }").append("\n");
+  }
 
-    public void addUnregisterData() {
-        this.data.append("        else if($status == 'unregister'){").append("\n")
-        .append("            try{").append("\n")
-        .append("                $con = new PDO('mysql:host='."+HOST+".';dbname='."+DB+","+DB_USER+","+DB_PASSWORD+");").append("\n")
-        .append("                $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);").append("\n")
-        .append("                $check_query = $con->prepare(\"select * from "+this.table+" where email1='\".$email.\"'\");").append("\n")
-        .append("                $check_query->execute();").append("\n")
-        .append("                if($check_query->rowCount() == 1){").append("\n")
-        .append("                    $register_query = $con->prepare(\"delete from "+this.table+" where email1=:email\");").append("\n")
-        .append("                    $register_query->bindParam(':email', $email);").append("\n")
-        .append("                    $status = $register_query->execute();").append("\n")
-        .append("                    if($status){").append("\n")
-        .append("                        echo json_encode( array('status' => 'unregistered'));").append("\n")
-        .append("                    }").append("\n")
-        .append("                    else").append("\n")
-        .append("                        echo json_encode( array('status' => 'error'));").append("\n")
-        .append("                }").append("\n")
-        .append("                else").append("\n")
-        .append("                    echo json_encode( array('status' => 'unregistered'));").append("\n")
-        .append("            }").append("\n")
-        .append("            catch(PDOException $e){").append("\n")
-        .append("                echo $e->getMessage();").append("\n")
-        .append("            }").append("\n")
-        .append("        }").append("\n");
-    }
+  public void addUnregisterData() {
+    this.data.append("        else if($status == 'unregister'){").append("\n")
+    .append("            try{").append("\n")
+    .append("                $con = new PDO('mysql:host='."+HOST+".';dbname='."+DB+","+DB_USER+","+DB_PASSWORD+");").append("\n")
+    .append("                $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);").append("\n")
+    .append("                $check_query = $con->prepare(\"select * from "+this.table+" where email1='\".$email.\"'\");").append("\n")
+    .append("                $check_query->execute();").append("\n")
+    .append("                if($check_query->rowCount() == 1){").append("\n")
+    .append("                    $register_query = $con->prepare(\"delete from "+this.table+" where email1=:email\");").append("\n")
+    .append("                    $register_query->bindParam(':email', $email);").append("\n")
+    .append("                    $status = $register_query->execute();").append("\n")
+    .append("                    if($status){").append("\n")
+    .append("                        echo json_encode( array('status' => 'unregistered'));").append("\n")
+    .append("                    }").append("\n")
+    .append("                    else").append("\n")
+    .append("                        echo json_encode( array('status' => 'error'));").append("\n")
+    .append("                }").append("\n")
+    .append("                else").append("\n")
+    .append("                    echo json_encode( array('status' => 'unregistered'));").append("\n")
+    .append("            }").append("\n")
+    .append("            catch(PDOException $e){").append("\n")
+    .append("                echo $e->getMessage();").append("\n")
+    .append("            }").append("\n")
+    .append("        }").append("\n");
+  }
 
-    public void addStatusData() {
-        this.data.append("        else if($status == 'getstatus'){").append("\n")
-        .append("            try{").append("\n")
-        .append("                $con = new PDO('mysql:host='."+HOST+".';dbname='."+DB+","+DB_USER+","+DB_PASSWORD+");").append("\n").append("\n")
-        .append("                $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);").append("\n")
-        .append("                $check_query = $con->prepare(\"select * from "+ this.table +" where email1='\".$email.\"'\");").append("\n")
-        .append("                $check_query->execute();").append("\n")
-        .append("                if($check_query->rowCount() == 1){").append("\n")
-        .append("                    echo json_encode( array('status' => registered));").append("\n")
-        .append("                }").append("\n")
-        .append("                else if($check_query->rowCount() == 0){").append("\n")
-        .append("                    echo json_encode( array('status' => unregistered));").append("\n")
-        .append("                }").append("\n")
-        .append("            }").append("\n")
-        .append("            catch(PDOException $e){").append("\n")
-        .append("                echo $e->getMessage();").append("\n")
-        .append("            }").append("\n")
-        .append("        }").append("\n")
-        .append("    }").append("\n")
-        .append("}").append("\n")
-        .append("?>").append("\n");
-    }
+  public void addStatusData() {
+    this.data.append("        else if($status == 'getstatus'){").append("\n")
+    .append("            try{").append("\n")
+    .append("                $con = new PDO('mysql:host='."+HOST+".';dbname='."+DB+","+DB_USER+","+DB_PASSWORD+");").append("\n").append("\n")
+    .append("                $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);").append("\n")
+    .append("                $check_query = $con->prepare(\"select * from "+ this.table +" where email1='\".$email.\"'\");").append("\n")
+    .append("                $check_query->execute();").append("\n")
+    .append("                if($check_query->rowCount() == 1){").append("\n")
+    .append("                    echo json_encode( array('status' => registered));").append("\n")
+    .append("                }").append("\n")
+    .append("                else if($check_query->rowCount() == 0){").append("\n")
+    .append("                    echo json_encode( array('status' => unregistered));").append("\n")
+    .append("                }").append("\n")
+    .append("            }").append("\n")
+    .append("            catch(PDOException $e){").append("\n")
+    .append("                echo $e->getMessage();").append("\n")
+    .append("            }").append("\n")
+    .append("        }").append("\n")
+    .append("    }").append("\n")
+    .append("}").append("\n")
+    .append("?>").append("\n");
+  }
 
 }
